6e5b35aed650dc4ea9b14d2929459b04c399891a
Vishal Sudasinghe <vishal.20240036@iit.ac.lk>
2025-07-04 10:14:31 +0530

    'done'

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,28 @@ const App = () => {
     .catch ((err) => console.log("Failed to fetch spells", err));
   }, [])
 
-  const getRandomSpell() {
+  const getRandomSpell = () => {
     const randomIndex = Math.floor(Math.random() * spells.length);
     setCurrentSpell(spells[randomIndex]);
   };
 
   return (
-    <div></div>
+    <div className="flex justify-center items-center h-[100vh]">
+      <div className=" w-[500px] h-[500px] bg-yellow-300">
+        <div>
+          <h1 className="font-bold ">Welcome to HOGWARTS</h1>
+        </div>
+        <button 
+        onClick={getRandomSpell}
+        className="bg-blue-500 text-white font-bold px-3 py-1 cursor-pointer shadow-lg rounded">Spell</button>
+        {currentSpell &&  (
+          <div className="">
+            <h1 className="font-bold">{currentSpell.name}</h1>
+            <p>{currentSpell.description}</p>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
